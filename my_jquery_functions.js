e2d9a87a9b0183b986e66df1a6b7584cbea945a6
kitkat119 <gprydal@yahoo.co.uk>
2017-08-02 17:07:45 +0100

    added jquery file

---
+++ my_jquery_functions.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+    alert( "welcome" );
+
+};
+$( document ).ready(function() {
+
+
+$(".power").click(function(){
+$ (".powerSaving").text()
+$ (".powerSaving").text("Power Saving mode is off ");
+});
+
+});
