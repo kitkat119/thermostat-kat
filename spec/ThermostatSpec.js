871b6f3528dc865bc68493e6849fdc4d86e62c1f
Alexander-Blair <zanderb@hotmail.co.uk>
2017-08-02 12:38:18 +0100

    Created power saving mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,8 +1,9 @@
 describe('Thermostat', function() {
-  var thermostat, defaultTemp;
+  var thermostat, defaultTemp, minimumTemp;
   beforeEach(function() {
     thermostat = new Thermostat();
     defaultTemp = 20;
+    minimumTemp = 10;
   });
 
   it('is created with temperature 20 degrees', function() {
@@ -15,5 +16,27 @@ describe('Thermostat', function() {
   it('can decrease the temperature by 1 degree', function() {
     thermostat.decreaseTemp();
     expect(thermostat.temperature()).toEqual(defaultTemp - 1);
+  });
+  it('does not reduce temperature below 10', function() {
+    while(thermostat.temperature() > minimumTemp) {
+      thermostat.decreaseTemp();
+    }
+    thermostat.decreaseTemp();
+    expect(thermostat.temperature()).toEqual(minimumTemp);
+  });
+  describe('power saving', function() {
+    it('is on by default', function() {
+      expect(thermostat.powerSaving()).toBeTruthy();
+    });
+    it('can be turned off', function() {
+      thermostat.togglePowerSaving();
+      expect(thermostat.powerSaving()).toBeFalsy();
+    });
+    it('can be turned on again', function() {
+      for(var i = 1; i <= 2; i++) {
+        thermostat.togglePowerSaving();
+      }
+      expect(thermostat.powerSaving()).toBeTruthy();
+    });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,6 @@
 function Thermostat() {
   this._temperature = 20;
+  this._powerSaving = true;
 };
 
 Thermostat.prototype.temperature = function() {
@@ -11,5 +12,13 @@ Thermostat.prototype.increaseTemp = function() {
 };
 
 Thermostat.prototype.decreaseTemp = function() {
-  this._temperature -= 1;
+  if(this._temperature > 10) { this._temperature -= 1; }
+};
+
+Thermostat.prototype.powerSaving = function() {
+  return this._powerSaving;
+};
+
+Thermostat.prototype.togglePowerSaving = function() {
+  this._powerSaving = !this._powerSaving;
 };
