f30983e8282474082fb4474fec99cc5db864e8b4
kitkat119 <gprydal@yahoo.co.uk>
2017-08-04 11:38:32 +0100

    user can select city to find out the local temp

--- my_jquery_functions.js
+++ my_jquery_functions.js
@@ -40,4 +40,18 @@ updateElements();
     thermostat.reset();
     updateElements();
   });
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+    
+  });
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=1165259d6e26ca0f8e03387e1f2da5ac';
+    var units = '&units=metric';
+    $.get(url + token + units, function(response) {
+      $('#current-temperature').text(response.main.temp);
+    });
+  };
 });
